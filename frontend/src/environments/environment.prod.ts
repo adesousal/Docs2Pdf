6d604e235097b7a1732fe9f6c46cbf7e3b2002e5
Ademir <ademir_lopes@hotmail.com>
2026-06-08 22:39:25 -0300

    tenso ngrok

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://8b40-2804-d55-5599-5700-a00-27ff-fe0c-c6f8.ngrok-free.app/api/convert'
+  apiUrl: 'https://e664-2804-d55-5599-5700-a00-27ff-fe0c-c6f8.ngrok-free.app/api/convert'
 };
